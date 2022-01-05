9a6113dc7884aedf285e0113773eeb68ef092fb3
Simone Peroni <wizard.simo@gmail.com>
2022-01-05 13:13:20 +0100

    RoadMap: further implementation of basic methods

--- include/rm/RoadMap.hpp
+++ include/rm/RoadMap.hpp
@@ -9,6 +9,8 @@ namespace rm
     class RoadMap
     {
     public:
+        typedef size_t node_id;
+
         class Node
         {
         public:
@@ -22,41 +24,56 @@ namespace rm
         private:
             static std::vector<Node> _all_nodes;
 
-            size_t _id;
+            node_id _id;
             Point _pos;
             std::vector<Orientation> _poses;
-            RoadMap *_parent;
+            std::vector<node_id> _connected;
 
         public:
-            static size_t create(Point pos);
-            static Node &getByID(size_t id);
-            static size_t getTotalNodeCount();
+            static node_id create(Point pos);
+            static Node &getByID(node_id id);
+            static node_id getTotalNodeCount();
 
             float getX() const;
             float getY() const;
-            size_t getID() const;
-            size_t getPosesCount() const;
-            Orientation &getPose(size_t id);
+            node_id getID() const;
+            node_id getPosesCount() const;
+            Orientation &getPose(node_id id);
+
+            void connectTo(node_id to);
+
+            operator node_id() const { return _id; }
 
         private:
-            Node(size_t id, Point pos);
-        };
+            Node(node_id id, Point pos);
+        }; // Node
 
         class Edge
         {
-        };
+        private:
+            node_id _from;
+            node_id _to;
+
+        public:
+            Edge(node_id from, node_id to);
+
+            node_id getFromID() const;
+            node_id getToID() const;
+            Node &getFromNode() const;
+            Node &getToNode() const;
+        }; // Edge
 
         class OrientationEdge
         {
-        };
+        }; // OrientationEdge
 
     private:
-        std::vector<Node *> _nodes;
-        std::vector<Edge *> _edges;
+        std::vector<node_id> _nodes;
+        std::vector<Edge> _edges;
         std::vector<OrientationEdge *> _orientEdge;
 
     public:
-        size_t addNode(Point pos);
-        void connect(size_t fromID, size_t toID);
-    };
+        node_id addNode(Point pos);
+        void connect(node_id fromID, node_id toID);
+    }; // RoadMap
 }
--- src/rm/RoadMap.cpp
+++ src/rm/RoadMap.cpp
@@ -3,16 +3,32 @@
 namespace rm
 {
     // RoadMap
-    size_t RoadMap::addNode(Point pos)
+    RoadMap::node_id RoadMap::addNode(Point pos)
     {
-        size_t id = Node::create(pos);
+        node_id id = Node::create(pos);
         if (id == Node::getTotalNodeCount() - 1)
-            _nodes.push_back(&Node::getByID(id));
+            _nodes.push_back(id);
         return id;
     }
 
+    void RoadMap::connect(node_id fromID, node_id toID)
+    {
+        _edges.push_back(Edge(fromID, toID));
+    }
+
+    // Edge
+    RoadMap::Edge::Edge(node_id from, node_id to) : _from(from), _to(to)
+    {
+        Node::getByID(from).connectTo(to);
+    }
+
+    RoadMap::node_id RoadMap::Edge::getFromID() const { return _from; }
+    RoadMap::node_id RoadMap::Edge::getToID() const { return _to; }
+    RoadMap::Node &RoadMap::Edge::getFromNode() const { return Node::getByID(_from); }
+    RoadMap::Node &RoadMap::Edge::getToNode() const { return Node::getByID(_to); }
+
     // Node
-    size_t RoadMap::Node::create(Point pos)
+    RoadMap::node_id RoadMap::Node::create(Point pos)
     {
         // Check if node exists
         for (const auto &n : _all_nodes)
@@ -21,21 +37,37 @@ namespace rm
                 return n.getID();
         }
 
-        size_t id = _all_nodes.size();
+        node_id id = _all_nodes.size();
         _all_nodes.push_back(Node(id, pos));
         return id;
     }
-    size_t RoadMap::Node::getTotalNodeCount() { return _all_nodes.size(); }
+    RoadMap::node_id RoadMap::Node::getTotalNodeCount() { return _all_nodes.size(); }
 
-    RoadMap::Node::Node(size_t id, Point pos) : _pos(pos), _id(id) {}
+    RoadMap::Node::Node(node_id id, Point pos) : _pos(pos), _id(id) {}
 
     float RoadMap::Node::getX() const { return _pos.x; }
 
     float RoadMap::Node::getY() const { return _pos.y; }
 
-    size_t RoadMap::Node::getID() const { return _id; }
+    RoadMap::node_id RoadMap::Node::getID() const { return _id; }
 
-    size_t RoadMap::Node::getPosesCount() const { return _poses.size(); }
+    RoadMap::node_id RoadMap::Node::getPosesCount() const { return _poses.size(); }
 
-    RoadMap::Node &RoadMap::Node::getByID(size_t id) { return _all_nodes.at(id); }
+    RoadMap::Node &RoadMap::Node::getByID(RoadMap::node_id id) { return _all_nodes.at(id); }
+
+    void RoadMap::Node::connectTo(node_id to)
+    {
+        if (to == _id)
+            return;
+        for (auto id : _connected)
+            if (id == to)
+                return;
+
+        _connected.push_back(to);
+    }
+
+    RoadMap::Node::operator node_id() const
+    {
+        return _id;
+    }
 }

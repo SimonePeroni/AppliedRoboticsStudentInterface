639eb52c6d3aa273420eec7c86e48e4d609c3a6e
mariotilocca98 <80837459+mariotilocca98@users.noreply.github.com>
2022-01-09 14:27:28 +0100

    Delete html directory

--- html/html/structrm_1_1_segment.js
+++
@@ -1,7 +0,0 @@
-var structrm_1_1_segment =
-[
-    [ "Segment", "structrm_1_1_segment.html#a7002927b310e88194b66b255b83e1b1c", null ],
-    [ "Segment", "structrm_1_1_segment.html#a1eabc3a1f90af53ab45520f4a8240732", null ],
-    [ "p0", "structrm_1_1_segment.html#a13ae68a1833bb23d9645a619c713c855", null ],
-    [ "p1", "structrm_1_1_segment.html#a6444507df562654bdcd3a09e25695cd4", null ]
-];

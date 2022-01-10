de00a01befa3861294a92bc7323d3139b0dd3ff2
Simone Peroni <wizard.simo@gmail.com>
2022-01-10 17:14:53 +0100

    visibility: added borders in computation

--- src/rm/visibility.cpp
+++ src/rm/visibility.cpp
@@ -36,6 +36,31 @@ namespace rm
                     }
                 }
             }
+            for (const Point &p0 : obstacles[i])
+            {
+                for (const Point &p1 : borders)
+                {
+                    bool visible = true;
+                    if (collisionCheck(p1, obstacles[i]) || collisionCheck(Segment(p0, p1), borders))
+                        continue;
+                    for (const Polygon &obst : obstacles)
+                    {
+                        if (collisionCheck(Segment(p0, p1), obst))
+                        {
+                            visible = false;
+                            break;
+                        }
+                    }
+
+                    if (visible)
+                    {
+                        auto n0 = roadmap.addNode(p0);
+                        auto n1 = roadmap.addNode(p1);
+                        roadmap.connect(n0, n1);
+                        roadmap.connect(n1, n0);
+                    }
+                }
+            }
         }
     }
 }

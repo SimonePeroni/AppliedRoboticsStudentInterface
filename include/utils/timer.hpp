f1d9c356d4f875253a706501ada5f4c0b3532a79
Simone Peroni <wizard.simo@gmail.com>
2022-01-14 13:48:26 +0100

    utils: added scope stack in Timer

--- include/utils/timer.hpp
+++ include/utils/timer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <vector>
 #include <string>
 
 namespace utils
@@ -8,11 +9,14 @@ namespace utils
     class Timer
     {
     private:
-        std::chrono::_V2::system_clock::time_point _tic;
+        std::vector<std::chrono::_V2::system_clock::time_point> _tic;
         std::chrono::duration<float, std::milli> _duration;
+
+        void indent();
+
     public:
         void tic(std::string msg = "");
-        void toc(std::string msg = " DONE");
+        void toc(std::string msg = "DONE");
         float millis();
     };
 }
--- src/utils/timer.cpp
+++ src/utils/timer.cpp
@@ -8,17 +8,33 @@ namespace utils
 {
     void Timer::tic(std::string msg)
     {
-        _tic = std::chrono::high_resolution_clock::now();
+        _tic.push_back(std::chrono::_V2::system_clock::time_point());
         if (!msg.empty())
+        {
+            indent();
             std::cout << msg << std::endl;
+        }
+        _tic.back() = std::chrono::high_resolution_clock::now();
     }
 
     void Timer::toc(std::string msg)
     {
         auto toc = std::chrono::high_resolution_clock::now();
-        _duration = toc - _tic;
+        _duration = toc - _tic.back();
         if (!msg.empty())
-            std::cout << msg << " [" << _duration.count() << " ms]" << std::endl;
+        {
+            indent();
+            std::cout << msg << " [" << millis() << " ms]" << std::endl;
+        }
+        _tic.pop_back();
+    }
+
+    void Timer::indent()
+    {
+        for (size_t i = 0; i < _tic.size(); i++)
+        {
+            std::cout << " ";
+        }
     }
 
     float Timer::millis()

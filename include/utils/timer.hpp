f7f727f1289aa39bbbb926fa799c374ecf7ecc0c
mariotilocca98 <80837459+mariotilocca98@users.noreply.github.com>
2022-01-15 11:54:45 +0100

    utils: timer added documentation

--- include/utils/timer.hpp
+++ include/utils/timer.hpp
@@ -1,5 +1,11 @@
 #pragma once
-
+/**
+ * @file timer.hpp 
+ * @brief file used to keep track of the time performance. 
+ * This file contains a simple timer used to chronometrize the run time of the various
+ * functions and operations done in the final implementation in student_interface.cpp
+ * 
+ */
 #include <chrono>
 #include <vector>
 #include <string>
@@ -9,6 +15,11 @@ namespace utils
     class Timer
     {
     private:
+     /**
+     * @brief The timer prints the amount of taken by a certain operation
+     * the final format is "DONE [n ms]" where n is the amount of milliseconds 
+     */
+
         std::vector<std::chrono::_V2::system_clock::time_point> _tic;
         std::chrono::duration<float, std::milli> _duration;
 

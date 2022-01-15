50c78396945bd661846a3050dc14ff04864524e5
Simone Peroni <wizard.simo@gmail.com>
2022-01-15 14:56:00 +0100

    inflate: updated documentation

--- include/rm/inflate.hpp
+++ include/rm/inflate.hpp
@@ -1,25 +1,26 @@
 #pragma once
-/**
- * @file inflate.hpp 
- * @brief Implementation of the obstacle inflation using the clipper library
- * clic on the function inflate to have more info about the input parameters and what is returned 
- * 
- */
 
 #include "clipper/clipper.hpp"
 #include "utils.hpp"
 
 #include <vector>
 
+/**
+ * @file inflate.hpp 
+ * @brief Implementation of the obstacle inflation using the clipper library
+ * 
+ * @see rm#inflate()
+ */
+
 namespace rm
 {
     /**
      * @brief Inflate polygons by specified offset value. 
      * 
-     * @param[in] polygons   Collection of original polygons to inflate.
-     * @param[in] offset     How much each polygon should be inflated.
-     * @param[in] clockwise  Whether the results should be given in clockwise order.
-     * @return               Collection of inflated polygons.
+     * @param polygons   Collection of original polygons to inflate.
+     * @param offset     How much each polygon should be inflated.
+     * @param clockwise  Whether the results should be given in clockwise order.
+     * @return           Collection of inflated polygons.
      */
     std::vector<Polygon> inflate(const std::vector<Polygon> &polygons, float offset, bool clockwise = false);
 }

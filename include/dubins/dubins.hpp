3602a0a2ebe678f90570f239ae32b0620789a65c
Simone Peroni <wizard.simo@gmail.com>
2022-01-15 12:22:33 +0100

    dubins: updated documentation

--- include/dubins/dubins.hpp
+++ include/dubins/dubins.hpp
@@ -1,227 +1,283 @@
 #pragma once
 /**
  * @file dubins.hpp
- * @brief Dubins implementation
- * In this file it is explained the implementation of the Dubins curves. All the material related to the equations employed has been taken from the professor's slides 
+ * @brief Library of functions for Dubins curves computation.\n
+ * 
+ * The maths used in this library is derived from the course material of "Robot Planning and its applications" at the University of Trento
  */
 
 #include <vector>
 #include <iostream>
 #include "utils.hpp"
 
+/**
+ * @brief This namespace holds the functions required for Dubins curves computation.
+ * 
+ */
 namespace dubins
 {
     /**
-     * @brief function used to implement the sinc(t)
+     * @brief Implementation of the sinc(t) function. The result is computed in a numerically stable way.
      * 
      * @param t 
-     * @return 1 if t is 1 otherwise  sin(t)/t
+     * @return 1 if t=1, sin(t)/t otherwise
      */
     float sinc(float t);
+
     /**
-     * @brief function used to normalize an angle 
+     * @brief Angle normalization in range [0:2pi).
      * 
-     * @param angle 
-     * @return float normalized angle in range [0:2pi]
+     * @param angle Angle to be normalized
+     * @return      Normalized angle in range [0:2pi)
      */
-
     float mod2pi(float angle);
-     /**
-     * @brief function used to normalize angular difference
+
+    /**
+     * @brief Angle normalization in range (-pi:pi].
      * 
-     * @param ang 
-     * @return float normalized angular difference in range[0:2pi]
+     * @param angle Angle to be normalized
+     * @return      Normalized angle in range (-pi:pi]
      */
+    float normAngle(float angle);
 
-    float normAngle(float ang);
-
-    // ---------------- Struct defining the position of the robot ---------------------------------------------------------------------------
-
-/**
- * @brief This structure represents the robot position in 2D with its orientation 
- * 
- */
+    /**
+     * @brief Structure representing a pose on the 2D xy-plane.
+     * 
+     */
     struct Pose2D
     {
+        /** First coordinate of the pose. */
         float x;
+        /** Second coordinate of the pose. */
         float y;
+        /** Angle of the pose, given counter-clockwise with respect to positive x-axis. */
         float theta;
     };
 
-    // ------------------ Struct for a Dubins arc -------------------------------------------------------------------------------------------
     /**
-     * @brief struct defining a dubins arc: it can be either a straight line or arc
-     * 
+     * @brief Structure representing an arc of the Dubins curve. The concept of arc here extends to straight lines, with 0 curvature.
+     * @see void setDubinsArc(DubinsArc, Pose2D, float, float)
      */
     struct DubinsArc
     {
-
-        Pose2D start; //--- inital robot pose coordinates
-        Pose2D end; //--- final robot pose coordinates
-        float k, s;
+        /** Initial pose */
+        Pose2D start;
+        /** Final pose */
+        Pose2D end;
+        /** Arc curvature */
+        float k;
+        /** Arc length */
+        float s;
     };
 
-    // --------------- Struct for a Dubins curve ---------------------------------------------------------------------------------------------
-     /**
-     * @brief Define the structure of a complete dubins curve
-     * @param arc_1
-     * @param arc_2
-     * @param arc_3
-     * @param  L total length of the curve 
-     * 
+    /**
+     * @brief Structure representing a Dubins curve connecting two poses in the 2D space.
+     * @see void setDubinsCurve(DubinsCurve, Pose2D, float, float, float, float, float, float)
      */
     struct DubinsCurve
     {
+        /** Length of the curve */
         float L;
+        /** First arc of the curve */
         DubinsArc arc_1;
+        /** Second arc of the curve */
         DubinsArc arc_2;
+        /** Third arc of the curve */
         DubinsArc arc_3;
     };
 
-/**
- * @brief checks the validity of a Dubins problem
- * It checks the validity of problem by evaluating the 3 equations
- * 
- * @param s1 original length of dubins curve
- * @param k0 original angle of the curve
- * @param s2 original length of dubins curve
- * @param k1 original angle of the curve
- * @param s3 original length of dubins curve
- * @param k2 original angle of the curve
- * @param th0 original inital angle 
- * @param thf original final angle 
- * @return true if the validity of all 3 equations is met
- * 
- */
+    /**
+     * @brief Checks the validity of a computed solution for the Dubins problem.
+     * 
+     * @param s1    Length of first arc
+     * @param k0    Curvature of first arc
+     * @param s2    Length of second arc
+     * @param k1    Curvature of second arc
+     * @param s3    Length of third arc
+     * @param k2    Curvature of third arc
+     * @param th0   Initial angle
+     * @param thf   Final angle
+     * @return      true if the computed solution is valid
+     * 
+     */
     bool check(float const &s1, float const &k0, float const &s2, float const &k1, float const &s3, float const &k2, float const &th0, float const &thf);
-    // -------- Scaling the functions ------------------------------------------
-	
-/**
-   * @brief Function to scale input problem to standard form 
-   * 
-   * @param start struct with inital robot pose
-   * @param end struct with final robot pose
-   * @param kmax maximum turning angle of the curve 
-   * @param sc_th_init scaled inital robot orientation
-   * @param sc_th_fin scaled final robot orientation 
-   * @param sc_kmax scaled maximum turning angle
-   * @param lambda 
-   */
-    void scaleToStandard(Pose2D start, Pose2D end, float kmax, float &sc_th_init, float &sc_th_fin, float &sc_kmax, float &lambda);
-	
-/**
- * @brief function used to scale the solution back to the original problem form 
- * 
- * @param lambda 
- * @param sc_s1 scaled length of dubins curve
- * @param sc_s2 scaled length of dubins curve
- * @param sc_s3 scaled length of dubins curve
- * @param s1 
- * @param s2 
- * @param s3 
- */
 
+    /**
+     * @brief Scale input parameters to the standard form.
+     * 
+     * @param start         Initial pose
+     * @param end           Final pose
+     * @param kmax          Maximum curvature
+     * @param sc_th_init    Out: Scaled inital orientation
+     * @param sc_th_fin     Out: Scaled final orientation 
+     * @param sc_kmax       Out: Scaled maximum curvature
+     * @param lambda        Out: Lambda value
+     * @see void scaleFromStandard(float, float, float, float, float, float, float);
+     */
+    void scaleToStandard(Pose2D start, Pose2D end, float kmax, float &sc_th_init, float &sc_th_fin, float &sc_kmax, float &lambda);
+
+    /**
+     * @brief Scale a solution in the standard form back to the original space. 
+     * 
+     * @param lambda    Lambda value
+     * @param sc_s1     Length of first arc of the Dubins curve in the standard form
+     * @param sc_s2     Length of second arc of the Dubins curve in the standard form
+     * @param sc_s3     Length of third arc of the Dubins curve in the standard form
+     * @param s1        Out: Length of first arc
+     * @param s2        Out: Length of second arc
+     * @param s3        Out: Length of third arc
+     * @see void scaleToStandard(Pose2D, Pose2D, float, float, float, float, float)
+     */
     void scaleFromStandard(float lambda, float sc_s1, float sc_s2, float sc_s3, float &s1, float &s2, float &s3);
 
-    // --- Dubins functions ----------------------------------------------------
-/**
- * @brief Implementation of one of the six Dubins primitives.
- * The same parameter and procedure followed for this equation has been applied to the remaining 5
- * 
- * @param sc_th_init scaled initial robot orientation
- * @param sc_th_fin scaled final robot orientation
- * @param sc_kmax scaled maximum turning angle 
- * @param ctrl is set to false if all the Dubins curves are 0 
- * @param sc_s1 scaled length of a Dubins curve
- * @param sc_s2 scaled length of a Dubins curve
- * @param sc_s3 scaled length of a Dubins curve 
- */
-
+    /**
+     * @brief Compute the RSR (right-turn - straight line - right-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void RSR(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
+    /**
+     * @brief Compute the RSL (right-turn - straight line - left-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void RSL(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
+    /**
+     * @brief Compute the RLR (right-turn - left-turn - right-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void RLR(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
+    /**
+     * @brief Compute the LSR (left-turn - straight line - right-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void LSR(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
+    /**
+     * @brief Compute the LSL (left-turn - straight line - left-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void LSL(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
+    /**
+     * @brief Compute the LRL (left-turn - right-turn - left-turn) Dubins primitive.
+     * 
+     * @param sc_th_init    Inital orientation in standard form
+     * @param sc_th_fin     Final orientation in standard form
+     * @param sc_kmax       Maximum curvature in standard form
+     * @param ctrl          Out: false if no feasible path was found
+     * @param sc_s1         Out: Length of first arc in the standard form
+     * @param sc_s2         Out: Length of second arc in the standard form
+     * @param sc_s3         Out: Length of third arc in the standard form
+     */
     void LRL(float sc_th_init, float sc_th_fin, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3);
 
-     /**
-     * @brief function used to compute a Dubins arc
+    /**
+     * @brief Create a DubinsArc object from the given parameters.
      * 
-     * @param ptr struct of a Dubins arc computed inside this function
-     * @param start struct defining the initial pose of the robot 
-     * @param k maximum turning angle 
-     * @param s total length of arc 
+     * @param ptr   Out: DubinsArc to be set
+     * @param start Initial pose
+     * @param k     Arc curvature
+     * @param s     Arc length
+     * @see struct DubinsArc
+     * @see struct DubinsCurve
      */
-
-
     void setDubinsArc(DubinsArc &ptr, const Pose2D &start, float k, float s);
-	/**
-     * @brief function used to compute a Dubins curve 
-     * 
-     * @param curve struct of a Dubins curve computed in this function 
-     * @param start initial 2D pose of the robot 
-     * @param s1 
-     * @param s2 
-     * @param s3 
-     * @param k0 
-     * @param k1 
-     * @param k2 
-     */
 
+    /**
+     * @brief Create a DubinsCurve object from the given parameters.
+     * 
+     * @param curve Out: DuvinsCurve to be set
+     * @param start Initial pose
+     * @param s1    First arc length
+     * @param s2    Second arc length
+     * @param s3    Third arc length
+     * @param k0    First arc curvature
+     * @param k1    Second arc curvature
+     * @param k2    Third arc curvature
+     * @see struct DubinsCurve
+     */
     void setDubinsCurve(DubinsCurve &curve, const Pose2D &start, float s1, float s2, float s3, float k0, float k1, float k2);
 
-    // ----- Find the shortest path ---------------------------------------------
-	/**
-     * @brief function used to find which is the shortest Dubins curve among the ones computed 
+    /**
+     * @brief Find the shortest Dubins curve connecting two poses in a 2D space.
      * 
-     * @param curve Dubins curve 
-     * @param start initial 2D pose of the robot
-     * @param end final 2D pose of the robot 
-     * @param kmax maximum turning angle 
-     * @param obstacles list of obstalces present in the map
-     * @param borders borders defining the map 
-     * @return true if the analyzed curve is the shortest one 
+     * @param curve     Out: Computed Dubins curve 
+     * @param start     Start pose
+     * @param end       End pose
+     * @param kmax      Maximum curvature
+     * @param obstacles Optional: vector of obstacles for collision check
+     * @param borders   Optional: vector of borders for collision check
+     * @return          true if a feasible curve was found, false otherwise
+     * @see struct DubinsCurve
      */
     bool findShortestPath(DubinsCurve &curve, Pose2D start, Pose2D end, float const &kmax, const std::vector<Polygon> &obstacles = std::vector<Polygon>(), const Polygon &borders = Polygon());
-	
-	/**
-     * @brief function used to calculate the robot pose 
-     * The calculation of the robot pose is obtained using the sin() function for the x and y coordinates and mod2pi 
-     * @param s length of the arc
-     * @param p0 
-     * @param k maximum turning angle of the arc
-     * @return Pose2D 
-     * @see float sinc(float t)
-     * @see float mod2pi(float angle)
+
+    /**
+     * @brief Compute the pose of a circular-arc trajectory at a given parameterized length.
+     * 
+     * @param s     Parameterized length. Must be in range [0, full arc length]
+     * @param p0    Initial pose of the arc
+     * @param k     Arc curvature
+     * @return      Pose of circular-arc trajectory at given s
      */
     Pose2D poseOnArc(float s, Pose2D p0, float k);
-    // ----- Discretization of the arc ------------------------------------------
-	/**
-     * @brief function used to discretize the arc
-     * number of points for the discretization calculated with rounding down (arc.s - offset) / step and adding  1 afterwards
-     * @param arc Dubins arc to be discretized
-     * @param s_end 
-     * @param step parameter used in the discretization 
-     * @param offset 
-     * @return std::vector<Pose> discretized arc 
+
+    /**
+     * @brief Discretize a Dubins arc.
+     * 
+     * @param arc       Dubins arc to be discretized
+     * @param step      Discretization step. Distance between two consecutive poses in the discretized path.
+     * @param offset    At what length of the arc to begin discretization. It is updated according to the remainder of the arc after discretization.
+     * @param path      Out: Vector of poses to which the discretized arc is appended
+     * @see void discretizeCurve(DubinsCurve, float, float, std::vector<Pose>)
      */
     void discretizeArc(const DubinsArc &arc, float step, float &offset, std::vector<Pose> &path);
-	
-     /**
-     * @brief function used to discretize the Dubins curve 
-     * The procedure described in the discretization of a Dubins arc is done for each arc of the curve and the results are combined together. 
-     * 
-     * @param curve 
-     * @param s_end 
-     * @param step 
-     * @param offset 
-     * @return std::vector<Pose> 
-     */
 
+    /**
+     * @brief Discretize a Dubins curve.
+     * 
+     * @param curve     Dubins curve to be discretized
+     * @param step      Discretization step. Distance between two consecutive poses in the discretized path.
+     * @param offset    At what length of the curve to begin discretization. It is updated according to the remainder of the curve after discretization.
+     * @param path      Out: Vector of poses to which the discretized curve is appended
+     * @see void discretizeArc(DubinsCurve, float, float, std::vector<Pose>)
+     */
     void discretizeCurve(const DubinsCurve &curve, float step, float &offset, std::vector<Pose> &path);
 } // dubins
--- src/dubins/dubins.cpp
+++ src/dubins/dubins.cpp
@@ -16,16 +16,19 @@ namespace dubins
 {
 	float check_threshold = 1e-4;
 
-	// ------- Sinc function -----------------------------------------------------
 	float sinc(float t)
 	{
+		if (t == 0.0f)
+			return 1;
 		if (abs(t) < 0.002)
-			return 1 - t * t / 120.f * (20.0f - t * t); // ---- Taylor series approximation
+		{
+			float t_sqr = t * t;
+			const float _1_120 = 1.f / 120;
+			return 1 - t_sqr * _1_120 * (20.0f - t_sqr);
+		}
 		else
 			return sin(t) / t;
 	}
-
-	// --------------- We need this function to normalize the angle in the range 0 to 360 deg --------------------------------
 
 	float mod2pi(float angle)
 	{
@@ -37,8 +40,6 @@ namespace dubins
 		return angle;
 	}
 
-	// --------------- We need this function to normalize the angle in the range -180 to 180 deg --------------------------------
-
 	float normAngle(float angle)
 	{
 		float p = 2.0f * M_PI;
@@ -49,7 +50,6 @@ namespace dubins
 		return angle;
 	}
 
-	// --------------------------------------
 	bool check(float const &s1, float const &k0, float const &s2, float const &k1, float const &s3, float const &k2, float const &th0, float const &thf)
 	{
 		float eq1 = s1 * sinc(0.5f * k0 * s1) * cos(th0 + 0.5f * k0 * s1) + s2 * sinc(0.5f * k1 * s2) * cos(th0 + k0 * s1 + 0.5f * k1 * s2) + s3 * sinc(0.5f * k2 * s3) * cos(th0 + k0 * s1 + k1 * s2 + 0.5f * k2 * s3) - 2.f;
@@ -62,7 +62,6 @@ namespace dubins
 
 		return tmp;
 	}
-	// ------------------------------------------------------------------------------------------------------------------------ //
 
 	void scaleToStandard(Pose2D start, Pose2D end, float kmax, float &sc_th0, float &sc_thf, float &sc_kmax, float &lambda)
 	{
@@ -75,8 +74,6 @@ namespace dubins
 		sc_kmax = kmax * lambda;
 	}
 
-	// --- These two functions are used to scale from and to the standard form
-
 	void scaleFromStandard(float lambda, float sc_s1, float sc_s2, float sc_s3, float &s1, float &s2, float &s3)
 	{
 		s1 = sc_s1 * lambda;
@@ -84,9 +81,6 @@ namespace dubins
 		s3 = sc_s3 * lambda;
 	}
 
-	// ------------------------------------------------------------------------------------------------------------------------- //
-
-	// ---------- Implementation of the primitives functions following the guidelines given by the professor in class --------------------
 	void LSL(float sc_th0, float sc_thf, float sc_kmax, bool &ctrl, float &sc_s1, float &sc_s2, float &sc_s3)
 	{
 		float invK = 1.0 / sc_kmax;
@@ -240,7 +234,6 @@ namespace dubins
 		}
 	}
 
-	// ----- Function to implement a structure representing a Dubins arc for both straight and circular ----------------------------------------------------
 	void setDubinsArc(DubinsArc &ptr, const Pose2D &start, float k, float s)
 	{
 
@@ -256,7 +249,6 @@ namespace dubins
 		ptr.end.theta = mod2pi(start.theta + k * s);
 	}
 
-	// ----- Function to implement a structure representing 3 arcs which make up the Dubins curve ------------------------------------------
 	void setDubinsCurve(DubinsCurve &curve, const Pose2D &start, float s1, float s2, float s3, float k0, float k1, float k2)
 	{
 
@@ -266,7 +258,6 @@ namespace dubins
 		curve.L = curve.arc_1.s + curve.arc_2.s + curve.arc_3.s;
 	}
 
-	// ----- Function to find the shortest path ---------------------------------------------------------------------------------------------
 	bool findShortestPath(DubinsCurve &curve, Pose2D start, Pose2D end, float const &kmax, const vector<Polygon> &obstacles, const Polygon &borders)
 	{
 		float sc_th0, sc_thf, sc_kmax;
@@ -292,7 +283,7 @@ namespace dubins
 			{1, -1, 1}}; //LRL
 
 		int pidx = -1;
-		double L = INFINITY; // Infinite value
+		double L = INFINITY;
 		bool ctrl;
 		float s1, s2, s3;
 		float sc_s1, sc_s2, sc_s3;
@@ -365,8 +356,6 @@ namespace dubins
 		out.theta = mod2pi(p0.theta + k * s);
 		return out;
 	}
-
-	//----------------- This last discretization step is needed in order to create smaller arcs that the robots will follow ----------------------
 
 	void discretizeArc(const DubinsArc &arc, float step, float &offset, std::vector<Pose> &path)
 	{

b79e7bff6f47e7159fa6e220950ba125ad07ea91
Simone Peroni <wizard.simo@gmail.com>
2022-01-15 15:31:38 +0100

    visibility: added documentation

--- include/rm/visibility.hpp
+++ include/rm/visibility.hpp
@@ -5,10 +5,43 @@
 
 #include <vector>
 
+/**
+ * @file visibility.hpp
+ * @brief This file is dedicated to visibility graph creation. \n 
+ * 
+ * @see rm#visibility()
+ * @see rm#makeVisibilityNodes()
+ */
+
 namespace rm
 {
+    /**
+     * @brief Compute a visibility graph from a given set of vertices. \n
+     * 
+     * Each couple of vertices are connected to eachother if the segment connecting them does not intersect any obstacles. 
+     * This is a naive O(E*V^2) implementation, where N is the number of vertices and E the number of edges in the obstacles.
+     * 
+     * @param roadmap   Out: The result is stored in the base directed graph of the roadmap.
+     * @param points    Vertices to be included in the graph
+     * @param obstacles Obstacles for collision checking
+     * @param borders   Borders of the arena for collision checking
+     * 
+     * @see rm#RoadMap
+     */
     void visibility(RoadMap &roadmap, const std::vector<Point> points, const std::vector<Polygon> &obstacles, const Polygon &borders);
 
+    /**
+     * @brief Generate a set of vertices for the visibility graph from the inflation of the obstacles. \n 
+     * 
+     * To avoid chunks of close vertices, a minimum distance threshold can be specified. 
+     * Vertices that are closer to each other than the threshold will be averaged into a single one.
+     * 
+     * @param obstacles Source obstacles
+     * @param borders   Borders of the arena, considered as obstacles
+     * @param offset    Inflation value of the obstacles.
+     * @param nodes     Out: vector of selected vertices
+     * @param threshold Optional: minimum distance between consecutive nodes
+     */
     void makeVisibilityNodes(const std::vector<Polygon> &obstacles, const Polygon &borders,
                              float offset, std::vector<Point> &nodes, float threshold = 0.0f);
 }
